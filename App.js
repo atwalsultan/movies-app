fddb2f45b4d3763ec59fa16c40493b228533b09c
Sultan Atwal <sultan.singh.atwal@gmail.com>
2021-10-03 15:29:18 -0700

    #1 feat: add home page header

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { NativeBaseProvider } from 'native-base';
+import Header from './components/layout/Header';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NativeBaseProvider>
+			<Header />
+			<StatusBar style='light' />
+		</NativeBaseProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
