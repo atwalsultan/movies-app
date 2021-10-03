6aa05e1cf16f2dd83cbe383051c8475e1045558f
Sultan Singh Atwal <58763741+atwalsultan@users.noreply.github.com>
2021-10-03 16:32:01 -0700

    #3 feat: add tab navigation (#4)

---
+++ components/stacks/SearchResultsStack.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "native-base"
+
+const SearchResultsStack = () => {
+    return (
+        <View>
+            <Text>Search Results</Text>
+        </View>
+    )
+}
+
+export default SearchResultsStack
